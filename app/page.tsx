888db9d2d5b8b1c7b940110a406b3add40af7c66
wirapramuja <wiraapramuja44@gmail.com>
2023-05-28 13:43:20 +0700

    update lagi dan lagi

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic'
+
 import Container from "@/app/components/Container";
 import ListingCard from "@/app/components/listings/ListingCard";
 import EmptyState from "@/app/components/EmptyState";
@@ -54,3 +56,4 @@ const Home = async ({ searchParams }: HomeProps) => {
 }
 
 export default Home;
+
